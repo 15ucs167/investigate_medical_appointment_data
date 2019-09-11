d6baf50e79c55c328d16045a0b332402e677cc83
Deepanshu Sharma <15ucs167@lnmiit.ac.in>
2019-09-11 16:36:54 +0530

    chore: Import necessary packages for analysis

--- investigate.py
+++ investigate.py
@@ -1,1 +1,7 @@
 #In this project, I'm investigating a dataset that contains 100,000 records of medical appointments in Brazil.
+
+#Importing the packages required for analysis
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
